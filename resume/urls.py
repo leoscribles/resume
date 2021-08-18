0d8fb26225f1ffb6e7790392bde3a7009b63e658
Leonard Uyanwanne <54281458+LeoX-commits@users.noreply.github.com>
2021-08-18 07:46:35 +0100

    first commit on local

---
+++ resume/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from .views import contactView, successView
+ 
+urlpatterns = [
+    path('', contactView, name='contact'),
+    path('success/', successView, name='success'),
+]
+
+
+
+
+    
